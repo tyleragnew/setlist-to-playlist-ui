75741ac83ae369892b5152398234b379adde6353
Tyler Agnew <tyler.agnew@gmail.com>
2024-12-01 15:08:27 -0500

    pkce flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { ArtistMetadata, ChooseArtist } from './pages/ChooseArtist';
+import { ChakraProvider } from '@chakra-ui/react'
+import { ArtistMetadata, ChooseArtist } from './pages/ChooseArtist'
 import { StepHeader } from './components/StepHeader';
 import { SetSetlistMetadata } from './pages/SetSetlistMetadata';
 
-import './App.css';
+import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ReviewPlaylist } from './pages/ReviewPlaylist';
 
 export const ListenerContext = React.createContext(null);
 
 function App() {
-  const [token, setToken] = useState('');
-  const clientId = 'd74b3ce0fbf342ecbfc8b32423800fa2';
+
+  const [token, setToken] = useState("")
+
+  const clientId = "d74b3ce0fbf342ecbfc8b32423800fa2";
   const params = new URLSearchParams(window.location.search);
-  const code = params.get('code');
-  const CALLBACK_URL = 'https://setlist-to-playlist-ui.vercel.app/callback';
+  const code = params.get("code");
+
+  const CALLBACK_URL = "https://setlist-to-playlist-ui.vercel.app/callback"
 
   useEffect(() => {
     if (!token) {
-      console.log('No Token');
+      console.log("No Token")
       if (!code) {
         redirectToAuthCodeFlow(clientId);
       } else {
@@ -27,31 +30,24 @@ function App() {
       }
 
       async function redirectToAuthCodeFlow(clientId: string) {
-        const verifier = generateCodeVerifier(256);
+        const verifier = generateCodeVerifier(128);
         const challenge = await generateCodeChallenge(verifier);
 
-        // Store the verifier in localStorage to use later for token exchange
-        localStorage.setItem('verifier', verifier);
+        localStorage.setItem("verifier", verifier);
 
         const params = new URLSearchParams();
-        params.append('client_id', clientId);
-        params.append('response_type', 'code');
-        params.append('redirect_uri', CALLBACK_URL);
-        params.append(
-          'scope',
-          'user-read-private user-read-email playlist-modify-public playlist-modify-private'
-        );
-        params.append('code_challenge_method', 'S256');
-        params.append('code_challenge', challenge);
+        params.append("client_id", clientId);
+        params.append("response_type", "code");
+        params.append("redirect_uri", CALLBACK_URL);
+        params.append("scope", "user-read-private user-read-email playlist-modify-public playlist-modify-private");
+        params.append("code_challenge_method", "S256");
+        params.append("code_challenge", challenge);
 
-        // Redirect to Spotify authorization URL
         document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
       }
 
-      // Function to generate the code verifier (random string)
       function generateCodeVerifier(length: number) {
-        const possible =
-          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
         let text = '';
         for (let i = 0; i < length; i++) {
           text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -59,7 +55,6 @@ function App() {
         return text;
       }
 
-      // Function to generate the code challenge from the code verifier
       async function generateCodeChallenge(codeVerifier: string) {
         const data = new TextEncoder().encode(codeVerifier);
         const digest = await window.crypto.subtle.digest('SHA-256', data);
@@ -69,28 +64,28 @@ function App() {
           .replace(/=+$/, '');
       }
 
-      // Function to exchange the authorization code for an access token
       async function getAccessToken(clientId: string, code: string) {
-        const verifier = localStorage.getItem('verifier');
+        const verifier = localStorage.getItem("verifier");
 
         const params = new URLSearchParams();
-        params.append('client_id', clientId);
-        params.append('grant_type', 'authorization_code');
-        params.append('code', code);
-        params.append('redirect_uri', CALLBACK_URL);
-        params.append('code_verifier', verifier!);
+        params.append("client_id", clientId);
+        params.append("grant_type", "authorization_code");
+        params.append("code", code);
+        params.append("redirect_uri", CALLBACK_URL);
+        params.append("code_verifier", verifier!);
 
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: params,
+        const result = await fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: params
         });
 
-        const data = await result.json();
-        setToken(data.access_token);
+        await result.json().then(res => {
+          setToken(res.access_token);
+        });
       }
     }
-  }, [code, token]);
+  }, []);
 
   const [chosenArtist, setChosenArtist] = useState<ArtistMetadata | null>();
   const [playlistMetadata, setPlaylistMetadata] = useState<any>();
@@ -98,33 +93,35 @@ function App() {
   const [setlistLoaded, setSetlistLoaded] = useState<false>();
 
   return (
-    <ListenerContext.Provider
-      //@ts-ignore
-      value={{
-        chosenArtist,
-        setChosenArtist,
-        token,
-        playlistMetadata,
-        setPlaylistMetadata,
-        setlistMetadata,
-        setSetlistMetadata,
-        setlistLoaded,
-        setSetlistLoaded,
-      }}
-    >
-      <ChakraProvider>
-        <StepHeader />
-        <br />
-        <BrowserRouter>
-          <Routes>
-            <Route index path="/callback" element={<ChooseArtist />} />
-            <Route path="/setlistMetadata" element={<SetSetlistMetadata />} />
-            <Route path="/playlist" element={<ReviewPlaylist />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    </ListenerContext.Provider>
-  );
+    <>
+      <ListenerContext.Provider
+        //@ts-ignore
+        value={{
+          chosenArtist,
+          setChosenArtist,
+          token,
+          playlistMetadata,
+          setPlaylistMetadata,
+          setlistMetadata,
+          setSetlistMetadata,
+          setlistLoaded,
+          setSetlistLoaded
+        }}>
+        <ChakraProvider>
+          <StepHeader />
+          <br />
+          <BrowserRouter>
+            <Routes>
+              <Route index path="/callback" element={<ChooseArtist />} />
+              <Route path="/setlistMetadata" element={<SetSetlistMetadata />} />
+              <Route path="/playlist" element={<ReviewPlaylist />} />
+            </Routes>
+          </BrowserRouter>
+        </ChakraProvider>
+      </ListenerContext.Provider >
+    </>
+  )
+
 }
 
-export default App;
+export default App
