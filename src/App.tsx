429de9b55fcf3303be89672dfab4c9719cb63ee8
Tyler Agnew <tyler.agnew@gmail.com>
2024-12-01 02:15:17 -0500

    lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
 import { ArtistMetadata, ChooseArtist } from './pages/ChooseArtist'
 import { StepHeader } from './components/StepHeader';
