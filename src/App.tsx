051c4402cec0c9fdb4d502abad4fd1704c20b4f7
Tyler Agnew <tyler.agnew@gmail.com>
2024-12-01 18:33:45 -0500

    spotify docs

--- src/App.tsx
+++ src/App.tsx
@@ -61,21 +61,29 @@ function App() {
 
     async function generateCodeVerifier(clientId: string) {
 
-      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      const randomValues = crypto.getRandomValues(new Uint8Array(64));
-      const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
+      const generateRandomString = (length: number) => {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+      }
+      
+      const codeVerifier  = generateRandomString(64);
+      
+      const sha256 = async (plain: any) => {
+        const encoder = new TextEncoder()
+        const data = encoder.encode(plain)
+        return window.crypto.subtle.digest('SHA-256', data)
+      }
 
-      const code_verifier = randomString;
-      const data = new TextEncoder().encode(code_verifier);
-      const hashed = await crypto.subtle.digest('SHA-256', data);
+      const base64encode = (input: any) => {
+        return btoa(String.fromCharCode(...new Uint8Array(input)))
+          .replace(/=/g, '')
+          .replace(/\+/g, '-')
+          .replace(/\//g, '_');
+      }
 
-      const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
-        .replace(/=/g, '')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_');
-
-      console.log("Setting Code Verifier");
-      window.localStorage.setItem('code_verifier', code_verifier);
+      const hashed = await sha256(codeVerifier)
+      const codeChallenge = base64encode(hashed);      
 
       const scope = "user-read-private user-read-email playlist-modify-public playlist-modify-private";
       const authUrl = new URL(authorizationEndpoint)
@@ -84,7 +92,7 @@ function App() {
         client_id: clientId,
         scope: scope,
         code_challenge_method: 'S256',
-        code_challenge: code_challenge_base64,
+        code_challenge: codeChallenge,
         redirect_uri: callbackURL,
       };
       authUrl.search = new URLSearchParams(params).toString();
