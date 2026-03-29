bb7f7061e8ae7ccbaf0da352496ac8854d1e7d01
Tyler Agnew <tyler.agnew@gmail.com>
2026-03-29 10:58:26 -0400

    fixing tests

--- src/tests/AppRoutes.test.tsx
+++ src/tests/AppRoutes.test.tsx
@@ -1,15 +1,33 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { ChooseArtist } from '../pages/ChooseArtist'
 import { StepProvider } from '../context/StepContext'
 import { ChakraProvider } from '@chakra-ui/react'
+import ListenerContext from '../context/ListenerContext'
+
+const mockContext = {
+    chosenArtist: null,
+    setChosenArtist: () => {},
+    token: '',
+    playlistMetadata: null,
+    setPlaylistMetadata: () => {},
+    setlistMetadata: null,
+    setSetlistMetadata: () => {},
+    setlistLoaded: false,
+    setSetlistLoaded: () => {},
+}
 
 test('ChooseArtist component renders header', () => {
     render(
-        <ChakraProvider>
-            <StepProvider>
-                <ChooseArtist />
-            </StepProvider>
-        </ChakraProvider>
+        <MemoryRouter initialEntries={["/"]}>
+            <ListenerContext.Provider value={mockContext}>
+                <ChakraProvider>
+                    <StepProvider>
+                        <ChooseArtist />
+                    </StepProvider>
+                </ChakraProvider>
+            </ListenerContext.Provider>
+        </MemoryRouter>
     )
 
     const header = screen.queryByText(/Choose Your Artist/i)
